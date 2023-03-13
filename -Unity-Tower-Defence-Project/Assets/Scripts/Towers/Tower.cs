843c4eb922fb79a15194a2cae79f9f79ded035eb
ikilinc1 <ikilinc1@binghamton.edu>
2023-03-13 12:30:12 -0400

    Core functionality for tower done

---
+++ -Unity-Tower-Defence-Project/Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public float range = 3f;
+    public LayerMask whatIsEnemy;
+
+    
+    public List<EnemyController> enemiesInRange = new List<EnemyController>();
+
+    public float checkTime = 0.2f;
+    private float checkCounter;
+    
+    private Collider[] collidersInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkCounter = checkTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkCounter -= Time.deltaTime;
+        if (checkCounter <= 0)
+        {
+            checkCounter = checkTime;
+            
+            collidersInRange = Physics.OverlapSphere(transform.position, range, whatIsEnemy);
+        
+            enemiesInRange.Clear();
+            foreach (Collider col in collidersInRange)
+            {
+                enemiesInRange.Add(col.GetComponent<EnemyController>());
+            }
+        }
+    }
+}
